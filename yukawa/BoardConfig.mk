05858fd0f96d19bf4e052a2f9705649373d14c8c
Joao <victor-souza1997@hotmail.com>
2023-09-16 00:24:00 -0400

    BoardConf.mk from yukawa has had the path changed

--- yukawa/BoardConfig.mk
+++ yukawa/BoardConfig.mk
@@ -1,7 +1,7 @@
-include device/amlogic/yukawa/BoardConfigCommon.mk
+include device/sidia/mojavedroid/BoardConfigCommon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_DEV_BOARD)
-TARGET_BOARD_INFO_FILE := device/amlogic/yukawa/board-info/board-info-$(TARGET_DEV_BOARD).txt
+TARGET_BOARD_INFO_FILE := device/sidia/mojavedroid/board-info/board-info-$(TARGET_DEV_BOARD).txt
 
 ifeq ($(TARGET_USE_AB_SLOT), true)
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 10730078208
