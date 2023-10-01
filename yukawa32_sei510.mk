f7f15b1174366c771a9e6fa62843a4ed00d63cd1
Joao <victor-souza1997@hotmail.com>
2023-10-01 11:37:06 -0400

    removed files from yukawa makefile

--- yukawa32_sei510.mk
+++
@@ -1,5 +0,0 @@
-$(call inherit-product, device/sidia/mojavedroid/device-yukawa_sei510.mk)
-$(call inherit-product, device/sidia/mojavedroid/yukawa-common.mk)
-
-PRODUCT_NAME := yukawa32_sei510
-PRODUCT_DEVICE := yukawa32_sei510
--- yukawa_sei510.mk
+++
@@ -1,7 +0,0 @@
-# Inherit the full_base and device configurations
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, device/sidia/mojavedroid/device-yukawa_sei510.mk)
-$(call inherit-product, device/sidia/mojavedroid/yukawa-common.mk)
-
-PRODUCT_NAME := yukawa_sei510
-PRODUCT_DEVICE := yukawa_sei510
